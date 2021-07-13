367e1b1e9c243f0574427983b283a754b72b71fa
mimi3403 <miimiixo@gmail.com>
2021-07-12 21:43:11 -0400

    adds model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Baby(models.Model):
+  name = models.CharField(max_length=100)
+  age = models.IntegerField()
+  gender = models.CharField(max_length=100)
+  personality = models.TextField(max_length=250)
