da1b62ec88ebb671ec321722efc159eee7b9605b
mimi3403 <miimiixo@gmail.com>
2021-07-13 23:11:19 -0400

    adds toy model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Baby(models.Model):
   name = models.CharField(max_length=100)
   age = models.IntegerField()
