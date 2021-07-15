a9c344985e59992252b0307be523f1dd04fcaf4e
mimi3403 <miimiixo@gmail.com>
2021-07-14 23:31:27 -0400

    adds diaper model

--- main_app/models.py
+++ main_app/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
-class Toy(models.Model):
-  name = models.CharField(max_length=100)
-
-  def __str__(self):
-    return self.name
-
-  def get_absolute_url(self):
-    return reverse('toys_detail', kwargs={'pk': self.id})
 
 class Baby(models.Model):
   name = models.CharField(max_length=100)
@@ -22,3 +14,31 @@ class Baby(models.Model):
   
   def get_absolute_url(self):
     return reverse('detail', kwargs={'baby_id' : self.id })
+
+class Toy(models.Model):
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+  class Diaper(models.Model):
+    CHANGING_TIMES = (
+      ('M', 'Morning'),
+      ('A', 'Afternoon'),
+      ('E', 'Evening'),
+    )
+
+    date = models.DateField()
+    changing_time = models.CharField(
+      max_length=1, 
+      choices=CHANGING_TIMES, 
+      default=CHANGING_TIMES[0][0]
+    )
+
+    baby = models.ForeignKey(Baby, on_delete=models.CASCADE)
+
+    def __str__(self):
+      return f'{self.get_changing_time_display()} on {self.date}'
