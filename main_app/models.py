de049afc72ea06cd2e9e742574d935db11dd3a33
mimi3403 <miimiixo@gmail.com>
2021-07-16 22:22:53 -0400

    adds user to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 # Create your models here.
 CHANGING_TIMES = (
   ('M', 'Morning'),
@@ -24,6 +25,7 @@ class Baby(models.Model):
   personality = models.TextField(max_length=250)
 
   toys = models.ManyToManyField(Toy)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
     return self.name
